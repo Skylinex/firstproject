32a7856d65ade906db7a763f2463badafe84a814
Black-ПК\Cawa <deshukr@gmail.com>
2017-12-21 11:37:45 +0200

    я создал

--- sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("да что сfdfdsfой не так");
+    System.out.println("да что сfdffdsfsdй не так");
   }
 
 }
